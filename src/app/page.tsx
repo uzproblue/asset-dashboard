c4d0aefa6797f5eeb3500dfcd509333bdb86c4b7
Farzona <farzonarakh@users.noreply.github.com>
2025-10-17 08:25:39 +0500

    Add the Header styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Header } from "@/components/Header";
 
 export default function HomePage() {
   return (
-    <main className="max-sm:w-343/375 max-md:w-648/744 min-xl:w-1200">
+    <main className="mx-auto max-sm:w-343/375 sm:w-638/744 lg:w-15/16 min-xl:w-300">
       <Header />
     </main>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export function Header() {
   return (
-    <header className="py-6 px-2.5 align-center">
-      <Image src={Logo} alt="Splint Invest logo" />
+    <header className="py-6">
+      <Image src={Logo} alt="Splint Invest logo" className="" />
     </header>
   );
 }
