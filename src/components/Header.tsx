e8c9c0602a3e1fe7bafe7933673c2e7a39aea584
uzproblue <uzproblue@gmail.com>
2025-10-16 22:28:23 -0300

    fixed logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,13 @@ export function Header() {
   return (
     <header className="py-4 px-2.5">
       <div className="max-w-7xl mx-auto">
-        <Image src={Logo} alt="Splint Invest logo" className="h-8" />
+        <Image
+          src={Logo}
+          alt="Splint Invest logo"
+          className="h-5"
+          width={134}
+          height={20}
+        />
       </div>
     </header>
   );
